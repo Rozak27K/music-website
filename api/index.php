686ecc8da2755ed7e081b824ff395bb9e008cbf2
Rozak27K <rozak7272277@gmail.com>
2026-08-30 19:58:09 +0700

    Bootstrap Laravel providers on Vercel

--- api/index.php
+++ api/index.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Bootstrap\BootProviders;
+use Illuminate\Foundation\Bootstrap\LoadConfiguration;
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
+use Illuminate\Foundation\Bootstrap\RegisterFacades;
+use Illuminate\Foundation\Bootstrap\RegisterProviders;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
@@ -53,6 +58,14 @@ try {
 
     /** @var Application $app */
     $app = require_once __DIR__.'/../bootstrap/app.php';
+
+    $app->bootstrapWith([
+        LoadEnvironmentVariables::class,
+        LoadConfiguration::class,
+        RegisterFacades::class,
+        RegisterProviders::class,
+        BootProviders::class,
+    ]);
 
     $app->handleRequest(Request::capture());
 } catch (Throwable $exception) {
